5bff97a718ec939260444b87707a18aef27cf562
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-10-28 21:14:00 +0000

    all3working

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,11 @@
 class GameController < ApplicationController
 
+  def home
+    render({ :template => "/gametemplates/home"})
+  end
+
+
+
   def play_rock
 
     cpu_selection = ["rock", "paper", "scissors"]
@@ -14,9 +20,40 @@ class GameController < ApplicationController
       @outcome = "lose"
     end
 
-
-
     render({ :template => "/gametemplates/play_rock"})
 
   end
+
+  def play_paper
+    cpu_selection = ["rock", "paper", "scissors"]
+
+    @cpu_move = cpu_selection.sample
+
+    if @cpu_move == "paper"
+      @outcome = "tied"
+    elsif @cpu_move == "rock"
+      @outcome = "won"
+    else
+      @outcome = "lose"
+    end
+
+    render({ :template => "/gametemplates/play_paper"})
+  end
+
+  def play_scissors
+    cpu_selection = ["rock", "paper", "scissors"]
+
+    @cpu_move = cpu_selection.sample
+
+    if @cpu_move == "scissors"
+      @outcome = "tied"
+    elsif @cpu_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "lose"
+    end
+
+    render({ :template => "/gametemplates/play_scissors"})
+  end
+
 end
